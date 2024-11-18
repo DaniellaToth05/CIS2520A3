57392f125716593d4862d57eba99157fb5ba4675
Daniella Toth <daniellatoth@me.com>
2024-11-17 20:35:48 -0500

    finished createNode function

--- a3q1_functions.c
+++ a3q1_functions.c
@@ -10,11 +10,43 @@ int varCount = 0;
 
 // The createNode function allocates memory to the tree and creates a new node using the given data before returning the node.
 Node* createNode(char *data){
+	
+	// check if data is not null
+	if(data !=NULL){
+
+		Node *newNode = malloc(sizeof(Node)); // allocate memory for the new node
+		
+		// check if memory allocation was successful
+		if(newNode != NULL){
+			int end = sizeof(newNode->data) - 1; // the last index of the data array (-1 because of the null terminator)
+			strncpy(newNode->data, data, end); // copy the data into the new node
+			newNode->data[end] = '\0';  // add the null terminator
+
+			// set the left and right pointers to null
+			newNode->left = NULL; 
+			newNode->right = NULL;
+			return newNode;
+		}
+		else{
+			printf("Error: memory allocation failed\n");
+			exit(1);
+		}
+	}
+	else{
+		printf("Error: data is null\n");
+		return NULL;
+	}
 	return NULL;
+	 
 }
 
 // The parseExpression function parses the expression string passed in from command line, stores the information in a new node, and returns the root node of the tree.
 Node* parseExpression(char *expr){
+	
+	
+	
+	
+	
 	return NULL;
 }
 
